9c4c35af8332bba02c1c6012680bf8c0d55bd953
qpzm <lhm1442@gmail.com>
2019-04-25 18:42:09 +0900

    Add gatsby-plugin-sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,8 @@ module.exports = {
       'We contribute a large-scale dataset of about 46K audio clips to human-written text pairs collected via crowdsourcing on the  AudioSet dataset.' +
       'We show that our collected captions are indeed faithful for audio inputs and discover what forms of audio representation and captioning models are effective for the audio captioning.',
     author: `Chris Dongjoo Kim, Byeongchang Kim, Hyunmin Lee, and Gunhee Kim NAACL-HLT 2019`,
-    googleSiteVerification: 'RqcB2uGlZIkp71SeZhAblt-vSMOk32X_kL_5elphKZ4'
+    googleSiteVerification: 'RqcB2uGlZIkp71SeZhAblt-vSMOk32X_kL_5elphKZ4',
+    siteUrl: 'https://audiocaps.github.io'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,7 +32,8 @@ module.exports = {
         //icon: ``, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
