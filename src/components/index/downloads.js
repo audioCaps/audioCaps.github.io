f728f1adec2f1f2dcb049c2f60f4e98550fa70c1
qpzm <lhm1442@gmail.com>
2019-05-03 19:28:04 +0900

    📱Fix downloads mobile responsive

--- src/components/index/downloads.js
+++ src/components/index/downloads.js
@@ -1,20 +1,28 @@
 import React from "react"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
 import Image from "./image"
 
 const Downloads = () => (
   <div id="downloads" className="container mb-5">
-    <h2>
-      Downloads <span className="soon">Comming soon!</span>
-    </h2>
-    <div className="images">
-      <Image
-        src="Database-icon-min.png"
-        label="Data"
-        link="/"
-        wrapperClass="icon"
-      />
-      <Image src="GitHub-min.png" label="Code" link="/" wrapperClass="icon" />
-    </div>
+    <h2>Downloads <span className="soon">&nbsp;soon!</span></h2>
+    <Row>
+      <Col sm>
+        <Image
+          src="Database-icon-min.png"
+          label="Data"
+          link="/"
+          wrapperClass="icon"
+        />
+      </Col>
+      <Col sm>
+        <Image
+          src="GitHub-min.png"
+          label="Code" link="/"
+          wrapperClass="icon"
+        />
+      </Col>
+    </Row>
   </div>
 )
 
