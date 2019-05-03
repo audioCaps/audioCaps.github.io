21c5c8890ad051ff46f7ca0b930ddfb7bcfddb82
qpzm <lhm1442@gmail.com>
2019-05-03 18:52:06 +0900

    📱 Fix acknowledge mobile responsive

--- src/components/index/acknowledge.js
+++ src/components/index/acknowledge.js
@@ -1,15 +1,23 @@
 import React from "react"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
 
-const images = ["SNUVL.png", "KAKAO.png", "NRF.jpeg"]
+const IMAGES = ["SNUVL.png", "KAKAO.png", "NRF.jpeg"]
+
+const Sponsor = ({ name }) => (
+  <Col className="sponsor" sm>
+    <img src={require(`../../images/${name}`)} alt={name} />
+  </Col>
+)
 
 const Acknowledge = () => (
   <div id="acknowlege" className="container mb-5">
     <h2>Acknowledgement</h2>
-    <div className="sponsors">
-      {images.map(i => (
-        <img src={require(`../../images/${i}`)} className={i} alt={i} key={i} />
+    <Row>
+      {IMAGES.map(filename => (
+        <Sponsor name={filename} key={filename} />
       ))}
-    </div>
+    </Row>
   </div>
 )
 
