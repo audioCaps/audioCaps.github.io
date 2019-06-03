37ed6a278af584472c483036f23650619cb9f080
Chris Dongjoo Kim <kimdon20@gmail.com>
2019-06-02 19:29:55 -0500

    add code and dataset link

--- src/components/index/downloads.js
+++ src/components/index/downloads.js
@@ -5,20 +5,20 @@ import Image from "./image"
 
 const Downloads = () => (
   <div id="downloads" className="container mb-5">
-    <h2>Downloads <span className="soon">&nbsp;soon!</span></h2>
+    <h2>Downloads</h2>
     <Row>
       <Col sm>
         <Image
           fileName="Database-icon-min.png"
           label="Data"
-          link="/"
+          link="https://github.com/cdjkim/audiocaps"
           wrapperClass="icon"
         />
       </Col>
       <Col sm>
         <Image
           fileName="GitHub-min.png"
-          label="Code" link="/"
+          label="Code" link="https://github.com/cdjkim/audiocaps"
           wrapperClass="icon"
         />
       </Col>
